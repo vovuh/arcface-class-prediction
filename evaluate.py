5099cba7e682f6bfc391f3863fbae59343d1cf51
Petrov Vladimir <petroff.97@yandex.ru>
2019-12-08 19:49:11 +0400

    removed useless utils

--- evaluate.py
+++ evaluate.py
@@ -3,24 +3,27 @@ import os
 import torch
 from tqdm import tqdm
 import numpy as np
+from torch.utils.data import DataLoader
 
 from models.model import ReSimpleModel
 
 from config.config import Config
 
-from create_data import read_data
+from lamp_dataset import lamp_dataset, resizeAndTensor
+
+import create_data
 
 def read_test_data():
-    testing_data = read_data()
+    testing_data = create_data.read_data()
     testing_image_paths = testing_data['imgpath'].tolist()
     return testing_image_paths
 
 
 def get_test_data_loader():
-    dataset = read_data()
-    return torch.utils.data.DataLoader(dataset, batch_size = Config.test_batch_size, shuffle = False, num_workers = Config.num_workers)
+	dataset = lamp_dataset(create_data.read_data(), transform = resizeAndTensor((224, 224)))
+	return DataLoader(dataset, batch_size = Config.test_batch_size, shuffle = False, num_workers = Config.num_workers)
 
-
+    
 def main(model_path, output_file_path):
     model = ReSimpleModel(bottleneck_size=Config.bottleneck_size).to(Config.train_device)
     model.load_state_dict(torch.load(model_path))
@@ -29,6 +32,9 @@ def main(model_path, output_file_path):
     img_size = (model.input_height, model.input_width)
 
     test_data = read_test_data()
+    with open('data_paths.txt', 'w') as f:
+    	f.writelines(["%s\n" % item  for item in test_data])
+    
     test_dl = get_test_data_loader()
 
     image_vectors = []
@@ -43,6 +49,6 @@ def main(model_path, output_file_path):
 
 if __name__ == '__main__':
     # this line required for using more than 1 worker in dataloader
-    #os.system(f"sudo mount -o remount,size=80G /dev/shm")
+    # os.system(f"sudo mount -o remount,size=80G /dev/shm")
 	
     main(model_path='checkpoints/general_v7_128_32_7.183936.h5', output_file_path='test_vectors.npy')
