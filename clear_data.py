860a5c63733a21e5b7b698e9e5c48f868e1d2648
unknown <petroff.97@yandex.ru>
2020-11-13 03:52:53 +0400

    config and cleaner changes

--- clear_data.py
+++ clear_data.py
@@ -6,6 +6,8 @@ from PIL import Image
 if __name__ == "__main__":
     list_bad = []
     for cpath, dirs, files in os.walk(os.path.join(os.getcwd(), 'dataset')):
+        if len(dirs) == 0 and len(files) < 3:
+            list_bad.append(cpath)
         for file in files:
             if file.endswith('.txt'):
                 continue
--- config/config.py
+++ config/config.py
@@ -3,14 +3,14 @@ import torch
 
 class Config(object):
     bottleneck_size = 128
-    num_classes = 110282
+    num_classes = 52384
 
     path_to_model = 'checkpoints'
 
     train_batch_size = 8
     test_batch_size = 60
 
-    num_workers = 4
+    num_workers = 0
 
     max_epoch = 50
 
