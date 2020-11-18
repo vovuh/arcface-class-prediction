c0ea6f26ebf22801d7434e43594fb623a9ff1dda
unknown <petroff.97@yandex.ru>
2020-11-18 21:54:21 +0400

    fixed classifier a bit

--- classifier.py
+++ classifier.py
@@ -4,31 +4,15 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
-import numpy as np
-import torchvision
-from torchvision import datasets, models, transforms
+from torchvision import models, transforms
 import matplotlib.pyplot as plt
 import time
-import os
 import copy
 from torch.utils.data import DataLoader
-from lamp_dataset import lamp_dataset, resizeAndTensor
+from lamp_dataset import lamp_dataset
 from sklearn.model_selection import train_test_split
 
 import create_data
-
-
-def imshow(inp, title=None):
-    """Imshow for Tensor."""
-    inp = inp.numpy().transpose((1, 2, 0))
-    mean = np.array([0.485, 0.456, 0.406])
-    std = np.array([0.229, 0.224, 0.225])
-    inp = std * inp + mean
-    inp = np.clip(inp, 0, 1)
-    plt.imshow(inp)
-    if title is not None:
-        plt.title(title)
-    plt.pause(0.001)  # pause a bit so that plots are updated
 
 
 def train_model(model, criterion, optimizer, scheduler, dataloaders, device, num_epochs=25):
@@ -99,33 +83,6 @@ def train_model(model, criterion, optimizer, scheduler, dataloaders, device, num
     return model
 
 
-def visualize_model(model, dataloaders, device, num_images=6):
-    was_training = model.training
-    model.eval()
-    images_so_far = 0
-    fig = plt.figure()
-
-    with torch.no_grad():
-        for i, (inputs, labels) in enumerate(dataloaders['val']):
-            inputs = inputs.to(device)
-            labels = labels.to(device)
-
-            outputs = model(inputs)
-            _, preds = torch.max(outputs, 1)
-
-            for j in range(inputs.size()[0]):
-                images_so_far += 1
-                ax = plt.subplot(num_images//2, 2, images_so_far)
-                ax.axis('off')
-                ax.set_title('predicted: {}'.format(class_names[preds[j]]))
-                imshow(inputs.cpu().data[j])
-
-                if images_so_far == num_images:
-                    model.train(mode=was_training)
-                    return
-        model.train(mode=was_training)
-
-
 if __name__ == "__main__":
     plt.ion()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -173,4 +130,3 @@ if __name__ == "__main__":
         exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
         model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, data_loaders, device)
-        visualize_model(model_ft, data_loaders, device)
